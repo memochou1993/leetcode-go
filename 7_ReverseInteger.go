04c65d202b7c394f2bb4e91a77c2693c656c1328
Memo Chou <memochou1993@gmail.com>
2020-02-15 22:30:25 +0800

    Solve problem

---
+++ 7_ReverseInteger.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	result := reverse(123)
+
+	fmt.Println(result)
+}
+
+func reverse(x int) int {
+	sign := 1
+
+	if x < 0 {
+		sign = -1
+		x *= -1
+	}
+
+	result := 0
+	for x > 0 {
+		temp := x % 10
+		result = result*10 + temp
+		x = x / 10
+	}
+
+	result *= sign
+
+	if result > math.MaxInt32 || result < math.MinInt32 {
+		result = 0
+	}
+
+	return result
+}
