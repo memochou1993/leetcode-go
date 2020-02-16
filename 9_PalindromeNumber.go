0f7bc749246fcbfc585f47f4a0195bafd84e2b81
Memo Chou <memochou1993@gmail.com>
2020-02-16 13:17:52 +0800

    Solve problem

---
+++ 9_PalindromeNumber.go
@@ -0,0 +1,17 @@
+package main
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+
+	copy := x
+	reverse := 0
+
+	for copy > 0 {
+		reverse = reverse*10 + copy%10
+		copy /= 10
+	}
+
+	return x == reverse
+}
